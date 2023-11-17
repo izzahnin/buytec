90790f446e61681607ba2ffe6b74201fd442bfa1
shobur <mfshobur@gmail.com>
2023-11-17 23:33:09 +0800

    feat: 1 - navbar (layout)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+// next.config.js
+module.exports = {
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      });
+      return config;
+    },
+  };
+  
+
 module.exports = nextConfig
