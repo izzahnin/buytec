1cc9042dab0e73481f80f2af59d3b9d410f4e319
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-11-18 15:54:18 +0800

    chore: font size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,7 +24,51 @@ const config: Config = {
         white: '#FFFFFF',
         black: '#1C1C1C',
         grey: '#F4F4F4',
-      }
+      },
+      fontSize: {
+        "text-s": [
+          "12px",
+          {
+            lineHeight: "16px",
+          },
+        ],
+        "text-m": [
+          "14px",
+          {
+            lineHeight: "20px",
+          },
+        ],
+        "text-l": [
+          "16px",
+          {
+            lineHeight: "24px",
+          },
+        ],
+        "heading-s": [
+          "20px",
+          {
+            lineHeight: "28px",
+          },
+        ],
+        "heading-m": [
+          "28px",
+          {
+            lineHeight: "36px",
+          },
+        ],
+        "heading-l": [
+          "36px",
+          {
+            lineHeight: "44px",
+          },
+        ],
+        "heading-xl": [
+          "64px",
+          {
+            lineHeight: "72px",
+          },
+        ],
+      },
     },
   },
   plugins: [],
