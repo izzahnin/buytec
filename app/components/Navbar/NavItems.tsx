46d18f97ac3f80601bf0bfafacc7d909607459d8
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-11-18 16:59:24 +0800

    feat: improve navbar

--- app/components/Navbar/NavItems.tsx
+++ app/components/Navbar/NavItems.tsx
@@ -1,19 +1,26 @@
 import Link from "next/link";
 import React, { useState } from "react";
-import { FaBeer } from 'react-icons/fa';
+
+import { MdSearch, MdOutlineShoppingCart, MdOutlinePersonOutline } from "react-icons/md";
 
 export default function NavItems() {
   const [active, setActive] = useState<number | null>(1);
 
   const navItems = [
-    { id: 1, title: "Notes", href: "/" },
+    { id: 1, title: "Notes", href: "/example" },
     { id: 2, title: "Brand", href: "/example" },
     { id: 3, title: "Occasion", href: "/example" },
     { id: 4, title: "Gender", href: "/example"},
+    { id: 5, title: "Concentration", href: "/example"},
+    { id: 6, title: "Size", href: "/example"},
+    { id: 7, title: "Origin", href: "/example"},
   ];
 
   return (
-    <section>
+    <nav className="sticky h-[70px] top-0 z-50 flex w-screen justify-between items-center shadow-lg px-5">
+      <Link href="/" className="font-playfair select-none text-primary-blue text-heading-l">
+        Elixir
+      </Link>
       <section className="flex gap-4 px-2 py-3 text-center">
         {navItems.map((item) => (
           <Link
@@ -28,9 +35,11 @@ export default function NavItems() {
           </Link>
         ))}
       </section>
-      <section>
-        {/* icon dipake dari react icon */}
+      <section className="flex text-heading-s gap-1">
+        <Link href="/" className="p-1"><MdSearch /></Link>
+        <Link href="/cart" className="p-1"><MdOutlineShoppingCart /></Link>
+        <Link href="/profile" className="p-1"><MdOutlinePersonOutline /></Link>
       </section>
-    </section>
+    </nav>
   );
 }
