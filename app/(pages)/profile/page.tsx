eefb495d08f502bc13026a2dd92d31a7d0da6e4d
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-11-18 16:59:44 +0800

    feat: create profile page

---
+++ app/(pages)/profile/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="flex w-screen h-screen justify-center items-center">
+    <h1>profile page</h1>
+  </div>
+  )
+}
