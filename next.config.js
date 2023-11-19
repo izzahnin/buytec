11e1175671edddd3ca6e32974c60848bdcd1719e
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-11-19 17:45:02 +0800

    chore: add image configuration

--- next.config.js
+++ next.config.js
@@ -1,18 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      { protocol: "https", hostname: "firebasestorage.googleapis.com" },
+    ],
+  },
+};
 
 // next.config.js
 module.exports = {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-        },
-      });
-      return config;
-    },
-  };
-  
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2)$/,
+      use: {
+        loader: "url-loader",
+      },
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
