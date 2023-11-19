d51d6e798930aea890d5f3c92bb6ca0497fa2c32
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-11-19 17:47:08 +0800

    feat: add product section

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,5 +1,7 @@
 // 'use client';
 import AboutSection from "@/components/AboutSection";
+import CardProduct from "@/components/ProductCard";
+import ProductSection from "@/components/ProductSection";
 import SectionCatalogue from "@/components/SectionCatalogue";
 import getAllPerfume from "@/firebase/parfume/getAllPerfume";
 
@@ -7,6 +9,7 @@ export default async function Home() {
   // const { logInWithGoogle } = useAuth();
   return (
     <main className="flex flex-col gap-10">
+      <ProductSection />
       <SectionCatalogue />  
       <AboutSection />
     </main>
