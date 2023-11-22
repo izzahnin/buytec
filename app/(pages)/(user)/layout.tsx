ea7d2305fc5f45c6c32d1bc13d60be9cf32a96d1
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-11-22 12:33:18 +0800

    feat: create layout for user

---
+++ app/(pages)/(user)/layout.tsx
@@ -0,0 +1,23 @@
+import { AuthContextProvider } from "@firebase/auth/AuthContext";
+import localFont from '@next/font/local'
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+const futura = localFont({
+  src:'../../../fonts/FuturaLT-Book.woff2',
+  display: 'swap',
+})
+
+export default function UserLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section  className={futura.className}>
+      <Navbar />
+      {children}
+      <Footer />
+    </section>
+  );
+}
