81b8f13cbde5c3ac80402e62d3b178907e5da04d
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-11-22 12:35:55 +0800

    feat: create admin page

---
+++ app/(pages)/(admin)/admin/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function admin() {
+  return (
+    <div>
+      Admin Page
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima sunt similique dolore consectetur. Culpa cumque laborum tempore. Nulla eaque fuga praesentium veritatis voluptas impedit. Architecto iure assumenda vel cum explicabo. Fuga qui ea autem repellat officia voluptatem minus nostrum enim delectus impedit inventore vel omnis, optio laudantium, odio veritatis corrupti illo nam. Beatae itaque voluptate autem natus rerum est inventore? Dolorum, ipsam quasi eligendi in deserunt doloremque obcaecati iure! Nesciunt eum et nisi? Nulla, optio adipisci porro, perferendis vitae cum odit voluptatibus totam accusamus amet voluptatem doloremque reiciendis! Quaerat officia nemo reiciendis et autem excepturi nihil cupiditate doloremque quos qui.</p>
+    </div>
+  )
+}
