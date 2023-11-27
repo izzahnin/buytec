4bc82415b93b0f36e80f229be24768b82422b3f0
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-11-27 10:59:08 +0800

    feat: resize font

--- app/components/AboutSection/index.tsx
+++ app/components/AboutSection/index.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function AboutSection() {
   return (
-    <main className='w-screen flex flex-col justify-center items-center'>
-      <section className='w-full'>
-        <Image className='w-full' src="/images/home__about-background.png" alt='aboutBackground' width={500} height={500}/>
-      </section>
+    <main className="flex h-fit w-screen flex-col items-center justify-center">
+      <Image
+        draggable={false}
+        className="relative w-full"
+        src="/images/home__about-background.png"
+        alt="aboutBackground"
+        width={500}
+        height={500}
+      />
 
-      <section className=' flex flex-col absolute text-center text-[18px] gap-[25px] text-white lg:text-[44px] lg:gap-[88px] md:text-[28px] md:gap-12 sm:text-[22px] sm:gap-10'>
-        <h1 className='font-medium'>About Us</h1>
-        <button className='font-medium border-white border-2 rounded-md'>More</button>
+      <section className="absolute flex w-1/4 flex-col items-center justify-center gap-2 text-center sm:text-heading-s text-white md:gap-4 md:text-heading-m lg:gap-6 lg:text-heading-l xl:gap-12">
+        <h1 className="font-semibold">About Us</h1>
+        <Link
+          href={"/about"}
+          className=" px-12 py-1 rounded-md border-2 border-white lg:text-heading-m font-medium transition-all  duration-300 ease-in-out hover:scale-105"
+        >
+          More
+        </Link>
       </section>
     </main>
-  )
+  );
 }
--- app/components/HeroSection/index.tsx
+++ app/components/HeroSection/index.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <section className="relative w-screen overflow-hidden bg-gradient-to-r from-dark-blue to-secondary-blue-accent">
-      <div className="absolute -top-[80%] right-[30%] hidden aspect-square w-full max-w-2xl rounded-full bg-secondary-blue-accent blur-2xl filter xl:block"></div>
-      <div className="absolute -bottom-[80%] right-[5%] hidden aspect-square w-full max-w-2xl rounded-full bg-secondary-blue-accent blur-2xl filter xl:block"></div>
-      <div className="flex items-center justify-center space-x-10 p-10 ">
+    <section className="relative w-full overflow-hidden bg-gradient-to-r from-dark-blue to-secondary-blue-accent">
+      <div className="absolute -top-[80%] right-[30%] hidden aspect-square w-full max-w-2xl rounded-full bg-secondary-blue-accent blur-2xl filter lg:block"></div>
+      <div className="absolute -bottom-[80%] right-[5%] hidden aspect-square w-full max-w-2xl rounded-full bg-secondary-blue-accent  blur-3xl filter lg:block"></div>
+      <div className="flex items-center justify-center gap-2 md:gap-6  p-10 ">
         <div className="relative aspect-[3/5] w-72">
           <Image
             src="/perfume3.png"
@@ -14,7 +14,7 @@ export default function HeroSection() {
             className="object-cover"
           />
         </div>
-        <h1 className="z-10 w-full max-w-4xl text-center font-playfair text-heading-s text-white md:text-heading-xl">
+        <h1 className="z-10 w-4/6 max-w-4xl text-center font-playfair text-heading-s sm:text-heading-m text-white md:text-heading-l lg:text-heading-xl select-none">
           Smell Fresh with Dior Newest Parfume
         </h1>
       </div>
