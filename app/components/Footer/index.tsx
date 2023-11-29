8c3c5838e038b8626ed1e15ee79a009dd64f2d55
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-11-29 22:59:22 +0800

    feat: imporve ui footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,79 +1,76 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 
-import { FaTwitterSquare, FaInstagram } from "react-icons/fa";
+import { navItems } from "@/components/Navbar/";
+
+import { FaTwitterSquare, FaInstagram, FaTiktok, FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <div className="flex w-full flex-col gap-12 bg-dark-blue px-8 py-8 text-white">
-      <div className="grid gap-10 lg:grid-cols-2 lg:gap-20 lg:px-40 lg:pt-24">
-        <div>
+    <footer className="flex w-full flex-col gap-12 bg-dark-blue  text-white">
+      <main className="flex flex-col sm:flex-row gap-8 justify-between mx-8 mt-4 md:mx-12 md:my-6 lg:mx-16 lg:mt-8">
+        <section className="flex flex-col gap-4">
           <Link
             href="/"
             className="select-none font-playfair text-heading-l text-white"
           >
             Elixir
           </Link>
-          <p className="select-none py-4 font-playfair text-white ">
-            Email: elixier@gmail.com
-          </p>
-          <p className="select-none py-4 font-playfair text-white ">
-            Phone: 00000000
-          </p>
-          <p className="select-none py-4 text-2xl text-white ">
-            Our Social Media
-          </p>
-
-          <div className="flex gap-6">
-            <FaInstagram size={30} />
-            <FaTwitterSquare size={30} />
+          <div className="flex select-none flex-col gap-2 text-white">
+            <span>Email: elixir@gmail.com</span>
+            <span>Phone: 0812-3456-7890</span>
           </div>
-        </div>
-
-        <div className="flex gap-24 lg:col-span-1">
-          <div>
+          <div className="flex flex-col select-none py-4 text-white gap-2 ">
+            <span>
+              Our Social Media
+            </span>
+            <div className="flex gap-2 items-center">
+              <a target="_blank" href={"https://www.instagram.com/"} className="bg-white p-1 text-dark-blue rounded-full">
+                <FaInstagram size={24} />
+              </a>
+              <a target="_blank" href={"https://twitter.com/"} className="bg-white p-1 text-dark-blue rounded-full">
+                <FaTwitter size={24} />
+              </a>
+              <a target="_blank" href={"https://www.tiktok.com/"} className="bg-white p-1 text-dark-blue rounded-full">
+                <FaTiktok size={24} />
+              </a>
+            </div>
+          </div>
+        </section>
+        <section className="flex md:w-1/4 md:justify-between gap-16">
+          <div className="flex flex-col gap-2">
             <h6 className="font-semibold">Categories</h6>
-            <ul>
-              <li className="py-2 text-sm duration-300 hover:cursor-pointer">
-                Notes
-              </li>
-              <li className="py-2 text-sm duration-300 hover:cursor-pointer">
-                Brand
-              </li>
-              <li className="py-2 text-sm duration-300 hover:cursor-pointer">
-                Occasion
-              </li>
-              <li className="py-2 text-sm duration-300 hover:cursor-pointer">
-                Gender
-              </li>
-              <li className="py-2 text-sm duration-300 hover:cursor-pointer">
-                Concentration
-              </li>
-              <li className="py-2 text-sm duration-300 hover:cursor-pointer">
-                Size
-              </li>
-              <li className="py-2 text-sm duration-300 hover:cursor-pointer">
-                Origin
-              </li>
-            </ul>
+            <section className="flex flex-col gap-1">
+              {navItems.map((item) => (
+                <Link
+                  key={item.id}
+                  href={item.href}
+                  className={` hover:font-semibold `}
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </section>
           </div>
-          <div>
+          <div  className="flex flex-col gap-2">
             <h6 className="font-semibold">Pages</h6>
-            <ul>
-              <li className="py-2 text-sm duration-300 hover:cursor-pointer">About Us</li>
-              <li className="py-2 text-sm duration-300 hover:cursor-pointer">All Product</li>
-            </ul>
+            <section className="flex flex-col gap-1">
+              <Link href={"/about"} className=" hover:font-semibold">
+                About Us
+              </Link>
+              <Link href={"/product"} className=" hover:font-semibold">
+                Product
+              </Link>
+            </section>
           </div>
-        </div>
-      </div>
-
-      <div className="flex flex-col items-center gap-12">
-        <div className="h-0.5 w-[99%] bg-white lg:w-11/12"></div>
-      </div>
-
-      <p className="text-start lg:text-center">
-        © 2023 Elixir Inc. All rights reserved
-      </p>
-    </div>
+        </section>
+      </main>
+      <footer className="flex justify-center border-t-2 py-6 select-none mx-8 md:mx-12 lg:mx-16">
+        <span className="text-center text-sm">
+          © 2023 Elixir Inc. All rights reserved
+        </span>
+      </footer>
+    </footer>
   );
 }
