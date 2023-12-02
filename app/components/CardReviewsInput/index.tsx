4555e39b27ec1dd4532742a914b0c21f3dd18835
Tsynblh <hasanuddintn21d@student.unhas.ac.id>
2023-12-02 13:55:59 +0800

    feat: improve UI on the CardReviewsInput component

--- app/components/CardReviewsInput/index.tsx
+++ app/components/CardReviewsInput/index.tsx
@@ -1,22 +1,42 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 export default function CardReviewsInput() {
   return (
     <main>
-        <section className='flex flex-row gap-5 shadow-lg p-5 rounded-xl lg:w-[712px]'>
-            <div>
-              <Image className='rounded-full w-12 sm:w-14 flex justify-center items-center' src="/images/profile.png" alt='PreviewUser' width={500} height={500}/>
-            </div>
+      <section className="flex flex-col flex-wrap gap-5 rounded-xl p-5 shadow-lg lg:w-[712px]">
+        <section className="flex flex-row gap-5">
+          <div>
+            <Image
+              className="flex w-12 items-center justify-center rounded-full sm:w-14"
+              src="/images/profile.png"
+              alt="PreviewUser"
+              width={500}
+              height={500}
+            />
+          </div>
 
-            <div>
-              <input className='mt-2 text-text-l w-60 sm:w-52 md:w-72 lg:w-96' type="text" name="review" id="" placeholder='Write your review...' />
-            </div>
-
-            <div className='self-end hidden sm:block md:block sm:pl-2 sm:pt-28'>
-              <button className='w-32 h-8 sm:w-[160px] sm:h-[35px] rounded-lg bg-primary-blue-accent font-black text-text-l text-white' type="submit">Submit</button>
-            </div>
+          <div className="flex-grow">
+            <input
+              style={{ width: '100%', wordWrap: 'break-word', overflowWrap: 'break-word' }}
+              className="mt-2 text-text-l outline-none"
+              type="text"
+              name="review"
+              id=""
+              placeholder="Write your review..."
+            />
+          </div>
         </section>
+
+        <div className="w-full self-end sm:ml-auto sm:w-auto sm:pl-2 sm:pt-2 md:w-auto">
+          <button
+            className="h-8 w-full rounded-lg bg-primary-blue-accent text-text-l font-black text-white sm:h-[35px] sm:w-[160px]"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
