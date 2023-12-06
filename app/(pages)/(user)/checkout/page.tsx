bc8330678f8625808d844781f60b569a09964e31
JetAriel <arielnoahswag@gmail.com>
2023-12-06 14:42:29 +0800

    feat: add checkout page

--- app/(pages)/(user)/checkout/page.tsx
+++ app/(pages)/(user)/checkout/page.tsx
@@ -1,12 +1,99 @@
+"use client";
 import CardOrderSummary from "@/components/CardOrderSummary";
-import React from "react";
+import CardPaymentMethod from "@/components/CardPaymentMethod";
+import { IoIosWarning } from "react-icons/io";
+import { useState } from "react";
+import Modal from "./Modal";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default function checkout() {
+  const [openModal, setOpenModal] = useState(false);
+
+  const [isRecaptchaVerified, setIsRecaptchaVerified] = useState(false);
+
+  const handleOpenModal = () => {
+    if (isRecaptchaVerified) {
+      setOpenModal(true);
+    } else {
+      alert("Please verify ReCAPTCHA first.");
+    }
+  };
+
+  const handleCloseModal = () => setOpenModal(false);
+
+  const handleRecaptchaChange = (value: any) => {
+    setIsRecaptchaVerified(true);
+    console.log("reCAPTCHA value:", value);
+  };
+
   return (
-    <main>
-      <div className="flex justify-center py-8">
-        <CardOrderSummary />
+    <main className="flex flex-col justify-center gap-4 px-12 py-8">
+      <h1 className="py-6 text-heading-l font-bold text-dark-blue">Checkout</h1>
+      <div className="flex w-auto flex-row items-center gap-8 rounded-md bg-yellow-500 p-4">
+        <IoIosWarning size="50px" />
+        <p className="text-heading-m">
+          Please fill your addres to continue place order
+        </p>
       </div>
+
+      <main className="flex w-full flex-col justify-between gap-8 lg:flex-row">
+        <section className="w-full rounded border-2 border-solid border-primary-blue-accent px-6 py-8 lg:w-4/6 ">
+          <h2 className="pb-8 text-heading-m font-bold">Payment Method</h2>
+          <div className="overflow-clip rounded border-2 border-solid border-primary-blue-accent px-6 py-8">
+            <h2 className="pb-8 text-heading-m font-bold">Bank</h2>
+            <div className="flex w-auto flex-col items-center gap-6 md:flex-row md:justify-start">
+              <div className="flex flex-col gap-6 sm:flex-row">
+                <div className="flex flex-col gap-6">
+                  <CardPaymentMethod bank={"BRI"} image={"/images/bri.svg"} />
+                  <CardPaymentMethod bank={"BCA"} image={"/images/bca.svg"} />
+                </div>
+                <div className="flex flex-col gap-6">
+                  <CardPaymentMethod bank={"BNI"} image={"/images/bni.svg"} />
+                  <CardPaymentMethod bank={"BSI"} image={"/images/bsi.svg"} />
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center gap-6">
+                <CardPaymentMethod
+                  bank={"Mandiri"}
+                  image={"/images/mandiri.svg"}
+                />
+                <CardPaymentMethod
+                  bank={"Permata"}
+                  image={"/images/permata.svg"}
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="w-full lg:w-2/6">
+          <div>
+            <CardOrderSummary />
+          </div>
+          <div className="flex justify-center py-8">
+            <ReCAPTCHA
+              sitekey="6LdFAScpAAAAAIm9ZJw4mxLUiFGNjtfext7LhxV9"
+              onChange={handleRecaptchaChange}
+            />
+          </div>
+          <div className="flex w-auto justify-center">
+            <button
+              className="h-16 w-72 rounded-lg bg-primary-blue-accent text-heading-m font-black text-white"
+              type="submit"
+              onClick={handleOpenModal}
+              disabled={!isRecaptchaVerified}
+            >
+              Place Order
+            </button>
+          </div>
+        </section>
+        {openModal && (
+          <>
+            <Modal closeModal={handleCloseModal} />
+          </>
+        )}
+      </main>
     </main>
   );
 }
--- app/components/CardPaymentMethod/index.tsx
+++ app/components/CardPaymentMethod/index.tsx
@@ -8,10 +8,10 @@ function CardPaymentMethod(props: CardProps) {
   const { bank, image} = props;
 
   return (
-    <section className="flex h-16 w-auto items-center gap-4 rounded-xl bg-white hover:border-primary-blue hover:border-2 p-4 text-lg font-medium drop-shadow-md">
-      <input type="radio"></input>
+    <section className="flex h-16 w-48 items-center gap-4 rounded-xl bg-white hover:border-2 hover:border-primary-blue p-4 text-lg font-medium drop-shadow-md">
+      <input type="radio" name="option"/>
       <p>{bank}</p>
-      <img src={image}></img>
+      <img src={image} />
     </section>
   );
 }
