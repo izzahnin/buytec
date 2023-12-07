e00f7d7516548da6810a7f9285d4a3bcf6cdd655
Syawal Achmad <syawalachmad1323@gmail.com>
2023-12-07 14:18:02 +0800

    feat: add whatsapp icon

--- app/(pages)/(user)/page.tsx
+++ app/(pages)/(user)/page.tsx
@@ -6,15 +6,29 @@ import ProductSection from "@/components/ProductSection";
 import SectionCatalogue from "@/components/SectionCatalogue";
 import getAllPerfume from "@/firebase/perfume/getAllPerfume";
 import getPerfumesByCategory from "@/firebase/perfume/getPerfumesByCategory";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   // const { logInWithGoogle } = useAuth();
   return (
-    <main className="flex flex-col gap-10 items-center">
-      <HeroSection />
-      <SectionCatalogue />
-      <ProductSection />
-      <AboutSection />
-    </main>
+    <div className="relative">
+      <main className="flex flex-col items-center gap-10">
+        <HeroSection />
+        <SectionCatalogue />
+        <ProductSection />
+        <AboutSection />
+      </main>
+
+      <Link href={"https://wa.me/+6281355938355"}>
+        <Image
+          src={"/whatsapp-icon.svg"}
+          alt="whatsappIcon"
+          width={50}
+          height={50}
+          className="fixed bottom-32 right-2"
+        />
+      </Link>
+    </div>
   );
 }
