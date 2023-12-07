bbcf46bc486d07e12bd59998a9af8dec3bc0a276
shobur <mfshobur@gmail.com>
2023-12-07 10:32:45 +0800

    fix: admin login

--- app/(pages)/(admin)/admin/page.tsx
+++ app/(pages)/(admin)/admin/page.tsx
@@ -1,5 +1,9 @@
 "use client";
-import { AdminLoginState, AuthAdminProvider, useAdmin } from "@/firebase/admin/AdminContext";
+import {
+  AdminLoginState,
+  AuthAdminProvider,
+  useAdmin,
+} from "@/firebase/admin/AdminContext";
 import { useAdminAuth } from "@/firebase/admin/useAdminAuth";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
@@ -10,61 +14,66 @@ export default function Admin() {
   const router = useRouter();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handleSubmit = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
     e.preventDefault();
-    await adminAuth.logIn(username, password);
-    console.log(adminAuth.loginState);
-    console.log(adminAuth.admin.username);
-    if (adminAuth.loginState == AdminLoginState.Success) {
-      router.push('/admin/product');
-    } else {
-      console.log('gagal login');
-    }
-  }
+    adminAuth.logIn(username, password).then(() => {
+      if (adminAuth.loginState == AdminLoginState.Success) {
+        router.push("/admin/product");
+      } else {
+        console.log("gagal login");
+      }
+    });
+  };
 
   return (
-      <main className=" flex h-screen w-screen items-center justify-center">
-        <section className="flex h-fit w-fit flex-col items-center justify-center gap-4 rounded-xl border px-12 py-6 shadow-xl ">
-          <h1 className="my-3 text-heading-m font-semibold">Admin</h1>
-          <section className="flex flex-col gap-4">
-            <div className=" flex w-full items-center gap-4 rounded-xl border-2  px-2">
-              <RxAvatar size={30} />
-              <input
-                type="text"
-                placeholder="username"
-                value={username}
-                onChange={(e) => {setUsername(e.target.value)}}
-                className="h-10 w-full bg-transparent outline-none"
-              />
-            </div>
-            <div className="flex w-full items-center gap-4 rounded-xl border-2  px-2">
-              <RxLockClosed size={30} />
-              <input
-                type="password"
-                onChange={(e) => {setPassword(e.target.value)}}
-                value={password}
-                placeholder="password"
-                className="h-10 w-full bg-transparent outline-none"
-              />
-            </div>
-          </section>
-          <button
-            //   onClick={async (e) => {
-            //     e.preventDefault();
-            //     const result = await auth.logIn(emailInput, passwordInput);
-            //     if (result != undefined) {
-            //       console.log(result.user.uid);
-            //       router.push('/');
-            //     }
-            //   }
-            // }
-            type="submit"
-            className="my-3 w-36 rounded-xl bg-primary-blue py-2 font-bold text-white"
-            onClick={handleSubmit}
-          >
-            Login
-          </button>
+    <main className=" flex h-screen w-screen items-center justify-center">
+      <section className="flex h-fit w-fit flex-col items-center justify-center gap-4 rounded-xl border px-12 py-6 shadow-xl ">
+        <h1 className="my-3 text-heading-m font-semibold">Admin</h1>
+        <section className="flex flex-col gap-4">
+          <div className=" flex w-full items-center gap-4 rounded-xl border-2  px-2">
+            <RxAvatar size={30} />
+            <input
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              className="h-10 w-full bg-transparent outline-none"
+            />
+          </div>
+          <div className="flex w-full items-center gap-4 rounded-xl border-2  px-2">
+            <RxLockClosed size={30} />
+            <input
+              type="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              value={password}
+              placeholder="password"
+              className="h-10 w-full bg-transparent outline-none"
+            />
+          </div>
         </section>
-      </main>
+        <button
+          //   onClick={async (e) => {
+          //     e.preventDefault();
+          //     const result = await auth.logIn(emailInput, passwordInput);
+          //     if (result != undefined) {
+          //       console.log(result.user.uid);
+          //       router.push('/');
+          //     }
+          //   }
+          // }
+          type="submit"
+          className="my-3 w-36 rounded-xl bg-primary-blue py-2 font-bold text-white"
+          onClick={handleSubmit}
+        >
+          Login
+        </button>
+      </section>
+    </main>
   );
 }
