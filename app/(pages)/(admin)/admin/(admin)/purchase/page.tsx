a4ce91ba9f207a030312afb2e4ef2ffe075662b3
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-12-14 18:59:00 +0800

    feat: add purchase

---
+++ app/(pages)/(admin)/admin/(admin)/purchase/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Purchase() {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>Purchase</div>
+  )
+}
--- app/(pages)/(admin)/admin/(admin)/shipping/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Shipping() {
-  return (
-    <div className='w-screen h-screen flex justify-center items-center'>Shipping</div>
-  )
-}
