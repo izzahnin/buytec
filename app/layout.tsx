530878e9c738aef604ef9388eff98acbd8345997
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-12-17 21:16:07 +0800

    feat:  add suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true}>
         {children}
         <Toaster position="top-right" />
       </body>
